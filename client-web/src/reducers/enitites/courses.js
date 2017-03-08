1e1f8c1ec0fb176d91c765fee485a3da667d0e80
chiptus <chiptus@gmail.com>
2017-03-08 14:19:28 +0200

    course reducer - remove deleted ingredients from course

--- client-web/src/reducers/enitites/courses.js
+++ client-web/src/reducers/enitites/courses.js
@@ -1,4 +1,24 @@
 import createEntityReducer from '../common/create-entity-reducer';
 
-import { COURSES } from '../../constants/actions';
-export default createEntityReducer(COURSES);
+import { COURSES, INGREDIENTS } from '../../constants/actions';
+
+const courseEntityReducer = createEntityReducer(COURSES);
+
+export default (state = {}, action) => {
+  if (!action.type || action.type.match(/^courses\//)) {
+    return courseEntityReducer(state, action);
+  }
+  switch (action.type) {
+    case INGREDIENTS.DELETE:
+      return Object.assign({}, state, {
+        items: Object.keys(state).map(courseId => {
+          return {
+            ...state[courseId],
+            ingredients: state[courseId].ingredients.filter(ing => ing._id !== action.payload._id)
+          }
+        })
+      })
+    default:
+      return state;
+  }
+}
