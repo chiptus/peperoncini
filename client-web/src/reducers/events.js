80311cef52b48092b2a61a033eb4d4436dc0860b
chiptus <chiptus@gmail.com>
2017-04-02 18:02:53 +0300

    remove dependency of events on ingredients

--- client-web/src/reducers/events.js
+++ client-web/src/reducers/events.js
@@ -1,11 +1,12 @@
-import {INGREDIENTS} from '../constants/actions';
-
 const defaultState = {
   isFetching: false,
   items: [],
 };
 
-export default function ingReducer(state = defaultState, {type, payload, error}){
+export default function ingReducer(
+  state = defaultState,
+  { type, payload, error }
+) {
   switch (type) {
     default:
       return state;
