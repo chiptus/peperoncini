b80920e73ffc1e275408e61edef3ed4699f2d99f
chiptus <chiptus@gmail.com>
2017-04-02 18:03:02 +0300

    remove sub-doc reducer

---
+++ client-web/src/reducers/common/utils.js
@@ -0,0 +1,12 @@
+export function removeSubDocFromState(state, key, idToRemove) {
+  return Object.keys(state).reduce(
+    (acc, itemId) => {
+      acc[itemId] = {
+        ...state[itemId],
+        [key]: state[itemId][key].filter(subItemId => subItemId !== idToRemove),
+      };
+      return acc;
+    },
+    {}
+  );
+}
