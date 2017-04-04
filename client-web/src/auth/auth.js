60cc52f1cba28cb2414e5e78d3738c225c3bdfe7
chiptus <chiptus@gmail.com>
2017-04-04 18:34:40 +0300

    env vars

--- client-web/src/auth/auth.js
+++ client-web/src/auth/auth.js
@@ -1,52 +1,44 @@
 import hello from 'hellojs';
 
 import request from 'superagent';
+import { SERVER_URL, FACEBOOK_CLIENT_ID } from '../config';
 
-const SERVER_URL = 'http://localhost:8080';
-const FACEBOOK_CLIENT_ID = '700042113498773';
+// const SERVER_URL = 'http://localhost:8080';
+// const FACEBOOK_CLIENT_ID = '700042113498773';
 
 let user = init();
 
 hello.init({
   facebook: FACEBOOK_CLIENT_ID,
-})
+});
 
 export default {
   isAuthenticated,
   login: () => {
-    hello('facebook')
-      .login()
-      .then(response => {
-        request
-          .post(`${SERVER_URL}/auth/facebook`)
-          .send({
-            socialToken: response.authResponse.access_token,
-          })
-          .set('Accept', 'application/json')
-          .then(
-          result => {
-            if (result.body && result.body.jwt) {
-              localStorage.setItem('user', JSON.stringify(result.body))
-            }
-            return result.body;
+    hello('facebook').login().then(response => {
+      request
+        .post(`${SERVER_URL}/auth/facebook`)
+        .send({
+          socialToken: response.authResponse.access_token,
+        })
+        .set('Accept', 'application/json')
+        .then(result => {
+          if (result.body && result.body.jwt) {
+            localStorage.setItem('user', JSON.stringify(result.body));
           }
-          )
-      });
+          return result.body;
+        });
+    });
   },
   test: () => {
-    
     if (!isAuthenticated()) {
-      console.error("not authenticated");
+      console.error('not authenticated');
       return Promise.reject('Not authenticated');
     }
-    
-    return request.get(`${SERVER_URL}/api/category?jwt=${user.jwt}`)
-      .end();
-  }
 
-
+    return request.get(`${SERVER_URL}/api/category?jwt=${user.jwt}`).end();
+  },
 };
-
 
 function init() {
   const userStr = localStorage.getItem('user');
--- client-web/src/config.js
+++ client-web/src/config.js
@@ -1,1 +1,8 @@
-export const SERVER_URL = 'http://localhost:8080';
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+// process.env.NODE_ENV === 'production'
+// ? process.env.REACT_APP_SERVER_URL
+// : process.env.REACT_APP_SERVER_URL_DEV;
+export const FACEBOOK_CLIENT_ID = process.env.REACT_APP_FACEBOOK_CLIENT_ID;
+// process.env.NODE_ENV === 'production'
+// ? process.env.REACT_APP_FB_APPID
+// : process.env.REACT_APP_FB_APPID_DEV;
