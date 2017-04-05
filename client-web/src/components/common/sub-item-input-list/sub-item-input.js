d1d6b05869082b423f41b7283a5c392805b33f21
chiptus <chiptus@gmail.com>
2017-04-05 18:21:15 +0300

    step and no negatives in sub-item-input

--- client-web/src/components/common/sub-item-input-list/sub-item-input.js
+++ client-web/src/components/common/sub-item-input-list/sub-item-input.js
@@ -2,25 +2,42 @@ import React, { PropTypes } from 'react';
 
 import { SelectField, MenuItem, TextField, FlatButton } from 'material-ui';
 
-
-const SubItemInput = ({ subItem, onChangeSubItemValue, subItems, onDelete, onChangeSubItem, subItemName }) => {
+const SubItemInput = (
+  {
+    subItem,
+    onChangeSubItemValue,
+    subItems,
+    onDelete,
+    onChangeSubItem,
+    subItemName,
+  }
+) => {
   return (
     <div>
       <SelectField
         style={{ verticalAlign: 'bottom' }}
         value={subItem._id}
         floatingLabelText={subItemName}
-        onChange={(e, key, selectedId) => onChangeSubItem(subItem._id, selectedId)} >
-        {
-          subItems.map(ing => <MenuItem value={ing._id} key={ing._id} primaryText={ing.name} />)
-        }
+        onChange={(e, key, selectedId) =>
+          onChangeSubItem(subItem._id, selectedId)}>
+        {subItems.map(ing => (
+          <MenuItem value={ing._id} key={ing._id} primaryText={ing.name} />
+        ))}
       </SelectField>
-      <TextField type="number"
+      <TextField
+        type="number"
         floatingLabelText="כמות"
         disabled={!subItem._id}
         value={subItem.value}
+        step="0.1"
         id={'ing' + subItem._id}
-        onChange={({ target: { value } }) => onChangeSubItemValue(subItem._id, value)} />
+        onChange={({ target: { value } }) => {
+          if (value < 0) {
+            return;
+          }
+          onChangeSubItemValue(subItem._id, value);
+        }}
+      />
       <FlatButton label="X" onTouchTap={() => onDelete(subItem._id)} />
     </div>
   );
@@ -34,10 +51,12 @@ SubItemInput.propTypes = {
   onChangeSubItemValue: PropTypes.func.isRequired,
   onChangeSubItem: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
-  subItems: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  })).isRequired
+  subItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default SubItemInput;
