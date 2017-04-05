e83e31fbb80fff002f18cc8750203d049ac3aae5
chiptus <chiptus@gmail.com>
2017-04-05 18:20:41 +0300

    use sub-item-input

--- client-web/src/components/courses/course-edit/ingredients-input.js
+++ client-web/src/components/courses/course-edit/ingredients-input.js
@@ -1,35 +1,48 @@
 import React, { PropTypes } from 'react';
 
-import CourseIngInput from './course-ing-input';
+import CourseIngInput from '../../common/sub-item-input-list/sub-item-input';
 
-const CourseIngredientsList = ({ ingredients, courseIngredients, onChangeIngredient, onChangeIngredientValue, onDelete }) => {
+const CourseIngredientsList = (
+  {
+    ingredients,
+    courseIngredients,
+    onChangeIngredient,
+    onChangeIngredientValue,
+    onDelete,
+  }
+) => {
   return (
     <div>
-      {
-        courseIngredients.map(
-          (ingredient, index) =>
-            <CourseIngInput
-              key={index}
-              {...{ onChangeIngredient, onChangeIngredientValue, onDelete, ingredients, ingredient }}
-            />
-        )
-      }
+      {courseIngredients.map((ingredient, index) => (
+        <CourseIngInput
+          key={index}
+          onChangeSubItem={onChangeIngredient}
+          onChangeSubItemValue={onChangeIngredientValue}
+          onDelete={onDelete}
+          subItem={ingredient}
+          subItemName={'רכיב'}
+          subItems={ingredients}
+        />
+      ))}
     </div>
   );
 };
 
-
 CourseIngredientsList.propTypes = {
-  ingredients: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    unit: PropTypes.string,
-    price: PropTypes.number
-  })),
-  courseIngredients: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string,
-    value: PropTypes.number,
-  })),
+  ingredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      unit: PropTypes.string,
+      price: PropTypes.number,
+    })
+  ),
+  courseIngredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      value: PropTypes.number,
+    })
+  ),
   onChangeIngredient: PropTypes.func.isRequired,
   onChangeIngredientValue: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
