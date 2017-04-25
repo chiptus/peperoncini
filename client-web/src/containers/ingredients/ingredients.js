20144624e8a69d6d9d250db3ec9b9c6c680002c6
chiptus <chiptus@gmail.com>
2017-04-25 17:29:53 +0300

    convert ingredients to use ItemsContainer

--- client-web/src/containers/ingredients/ingredients.js
+++ client-web/src/containers/ingredients/ingredients.js
@@ -1,47 +1,28 @@
-import React, { Component, PropTypes } from 'react';
-
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { connect } from 'react-redux';
+import { func } from 'prop-types'
+import ItemsContainer from '../common/items-container'
 import IngredientsPage from './ingredients-page';
 import AddIngredientPage from './add-ingredient-page';
 
-import { connect } from 'react-redux';
-import { fetchItemsIfNeeded } from '../../actions/ingredients';
+import { fetchItemsIfNeeded } from '../../actions/ingredients'
 
-class IngredientsContainer extends Component {
-  componentWillMount() {
-    this.props.fetchItems();
-  }
 
-  render() {
-    const { isFetching } = this.props;
-    return (
-      <div>
-        {isFetching ? null :
-          <Switch>
-            <Route path="/ingredients/edit/:id" render={({ push, match: { params: { id } } }) => <AddIngredientPage {...{ push, id }} />} />
-            <Route path="/ingredients/add" render={({ push }) => <AddIngredientPage {...{ push }} />} />
-            <Route path="/ingredients/" render={({ push }) => <IngredientsPage {...{ push }} />} />
-          </Switch>
-        }
-      </div>
-    );
-  }
-}
+const IngredientsContainer = ({ fetchItems, isFetching }) => (
+  <ItemsContainer {...{ fetchItems, isFetching }} itemsPage={IngredientsPage} addItemPage={AddIngredientPage} itemsName="ingredients" />
+)
 
 IngredientsContainer.propTypes = {
-  isFetching: PropTypes.bool,
-  items: PropTypes.array,
+  fetchItems: func.isRequired,
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return ({
-    // items: !state.ingredients.isFetching ? state.ingredients.items.map(id => state.entities.ingredients[id]) : [],
-    isFetching: state.ingredients.isFetching,
-  })
-}
+const mapStateToProps = (state, ownProps) => ({
+  isFetching: state.ingredients.isFetching && state.courses.isFetching,
+});
 
-const mapActionsToProps = (dispatch, ownProps) => ({
-  fetchItems: () => dispatch(fetchItemsIfNeeded()),
-})
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  fetchItems: () => dispatch(fetchItemsIfNeeded())
+  // .then(dispatch(fetchIngredientsIfNeeded()))
+});
 
-export default connect(mapStateToProps, mapActionsToProps)(IngredientsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(IngredientsContainer);
