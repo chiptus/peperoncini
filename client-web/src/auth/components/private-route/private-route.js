912f9c9b7a55452faa4d2ffc61d04fe6d2802e9b
chiptus <chiptus@gmail.com>
2017-05-03 13:34:01 +0300

    prettier private-route

--- client-web/src/auth/components/private-route/private-route.js
+++ client-web/src/auth/components/private-route/private-route.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import { connect } from 'react-redux'
-
+import { connect } from 'react-redux';
 
 const PrivateRoute = ({ component, render, isLoggedIn, ...rest }) => (
-  <Route {...rest} render={props => (
-    isLoggedIn ?
-      component ? React.createElement(component, props) : render(props)
-      : (
-        <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }} />
-      )
-  )} />
-)
+  <Route
+    {...rest}
+    render={props =>
+      (isLoggedIn
+        ? component ? React.createElement(component, props) : render(props)
+        : <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />)}
+  />
+);
 
 const mapStateToProps = (state, ownProps) => ({
-  isLoggedIn: !!state.auth.token
-})
+  isLoggedIn: !!state.auth.token,
+});
 
 export default connect(mapStateToProps)(PrivateRoute);
