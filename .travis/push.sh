6c4a1b7d5074f9706fda4a653817ebe39c50655c
Chaim Lando <chiptus@gmail.com>
2017-12-14 16:51:07 -0800

    update github token

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/chiptus/peperoncini.git
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/chiptus/peperoncini.git
   git push --quiet --set-upstream origin-pages heroku 
 }
 
